8e89c8d62370c3b14d44e6b4f84e27a986587e7a
SeanKim05 <dbgus20609@gmail.com>
2024-08-09 17:04:08 +0900

    Design: 홈페이지 작업

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,27 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      white: "#ffffff",
+      "violet/600": "#7C3AED",
+      "violet/100": "#EDE9FE",
+      "rose/500": "#F43F5E",
+      "lime/300": "#BEF264",
+      "amber/800": "#92400E",
+      "green/700": "#15803d",
+      "slate/900": "#0F172A",
+      "slate/800": "#1E293B",
+      "slate/500": "#6478B",
+      "slate/400": "#94A3B8",
+      "slate/300": "#CBD5E1",
+      "slate/200": "#E2E8F0",
+      "slate/100": "#F1F5F9",
+    },
+
+    fontFamily: {
+      nanumB: ["NanumBold", "sans-serif"],
+      nanumR: ["NanumRegular", "sans-serif"],
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
